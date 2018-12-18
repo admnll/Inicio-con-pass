43df2a3d249e467f274c63f94e1a241a0550c7a6
Ed Basaldua <basalduadmnll@gmail.com>
2018-12-17 18:55:30 -0600

    Configurando todos los modulos en el servidor

--- src/server.js
+++ src/server.js
@@ -8,21 +8,47 @@ const flasheo = require('connect-flash');
 const morgana = require('morgan');
 const galleta = require('cookie-parser');
 const modulin = require('body-parser');
-const sesion = require(express-session);
+const sesion = require('express-session');
 
+const { url } = require('./config/database');
 
-// Configuraciones
+manguito.connect(url, {
+    // useMongoClient: true  //Esto ya no sirve para versiones superiores a 5.x
+    useNewUrlParser: true
+});
+
+// Para poder usar passport
+require('./config/passport')(pasaporte);
+
+    // Configuraciones
 aplicacion.set('port', process.env.PORT || 3000 );
 
+    // Las vistas están en la carpeta views
+aplicacion.set('views', ruta.join(__dirname, 'views')); 
+
+    //Motor de plantillas EJS
+aplicacion.set('view engine', 'ejs'); 
 
 // Middlewares
+    // Usaremos Morgan para ver los mensajes por consola    
+aplicacion.use(morgana('dev')); 
+aplicacion.use(galleta());
+aplicacion.use(modulin.urlencoded({extended: false}));
+aplicacion.use(sesion({
+    secret: 'passecreto',
+    resave: false,
+    saveUninitialized: false
 
+}));
+aplicacion.use(pasaporte.initialize);
+aplicacion.use(pasaporte.session);
+aplicacion.use(flasheo()); // Sirve para pasar mensajes entre HTMLs
 
 // Rutas
+require('./app/routes')(aplicacion, pasaporte);
 
-
-// Archivos estáticos
-
+// Archivos estáticos (HTML, CSS imagenes, etc)
+aplicacion.use(cafesito.static(ruta.join(__dirname, 'public')));
 
 aplicacion.listen(aplicacion.get('port'), () => {
     console.log('Servidor en el puerto', aplicacion.get('port'));
